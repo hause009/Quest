3405876d872613607a860a6cace5309a7a79ea7b
Admin <admin@Admins-Mac.local>
2014-07-02 15:39:53 +0400

    Initial Commit

---
+++ Quest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Quest
+//
+//  Created by Admin on 02.07.14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
